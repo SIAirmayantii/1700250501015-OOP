71d0554b58115c242a9a89749e0bc41fc04d2374
SIAirmayantii <57218125+SIAirmayantii@users.noreply.github.com>
2019-11-10 22:26:27 +0700

    Add files via upload

---
+++ kubus.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.kubus;
+
+/**
+ *
+ * @author La Tahzan
+ */
+public class kubus {
+public static void main (String[] args){
+   int volume,permukaan ;
+ 
+   System.out.println("diketahui luas salah satu sisi adalah 25");
+   
+   volume = (25*25*25);
+   permukaan = (6*25*25);
+   System.out.println("ditanyakan Luas permukaan kubus dan volume kubus");
+   
+   System.out.println("Luas permukaan kubus adalah " + permukaan);
+   System.out.println("volume kubus adalah " + volume);
+}
+}
---
+++ luassegitiga.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.luassegitiga;
+
+/**
+ *
+ * @author La Tahzan
+ */
+public class luassegitiga {
+public static void main (String[] args){
+   double tinggi,alas,luas;
+   tinggi = 7;
+   alas = 6;
+   luas = (alas * tinggi)/2;
+   System.out.println("tinggi segitiga Adalah " + tinggi);
+   System.out.println("alas Segitiga Adalah " + alas);        
+   System.out.println("Luas Segitiga Adalah " + luas);
+   
+}
+}
