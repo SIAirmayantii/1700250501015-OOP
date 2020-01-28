807b6c3d5238d0fd3e3c638e820ed82cf8bdf35c
SIAirmayantii <57218125+SIAirmayantii@users.noreply.github.com>
2020-01-28 11:58:52 +0700

    Add files via upload

---
+++ ceknilai.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+public class ceknilai {
+    public static void main(String[] args) {
+        int nilai=70;
+            if(nilai>=70 && nilai<=100){
+                System.out.println("Lulus");
+            }else if (nilai>=0 && nilai<70){
+                System.out.println("Tidak Lulus");
+            }else{
+                System.out.println("Inputkan nilai 0-100");
+            }
+     }
+}
+
